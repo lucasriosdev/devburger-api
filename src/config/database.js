ef0577ee99bfb9ede6ec6e0a3109e0fb0fb690e0
Lucas Rios <lucasriosdev@gmail.com>
2025-04-05 22:59:30 -0300

    Instalando o Sequelize e Configurando o banco

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+export default {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'devburger',
+
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
