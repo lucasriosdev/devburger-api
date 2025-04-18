d961d3f3eb58e18df93e970335d820a3e45383cb
Lucas Rios <lucasriosdev@gmail.com>
2025-04-17 21:16:32 -0300

    Configuração do Model de Usuário

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/controllers/models/User.js';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-  return response.status(200).json({ message: 'Hello World!' });
-})
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Lucas',
+    email: 'lucasriosdev.com.br',
+    password_hash: '123456',
+  });
+  return response.status(201).json(user);
+});
 
-export default routes
+export default routes;
